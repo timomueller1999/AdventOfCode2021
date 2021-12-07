97f58aaed5652e2a047ccb5a8d383cb77d559985
justus <j-gk@gmx.net>
2021-12-07 11:44:46 +0100

    Solved day 5 part 2
    Commit changes how diagonals are treated in file 05-2-prog.py. There was a logical error before which I did not find. Now the program proceedes differently. Apparently, diagonals are now treated correctly.

--- 05-2-prog.py
+++ 05-2-prog.py
@@ -1,12 +1,12 @@
 import pdb  # Python debugger
 import numpy as np
 
-with open('05-1-sample_input.txt', 'r') as f:
+with open('05-1-input.txt', 'r') as f:
     stri = f.read()
 
 vent_list = []
-x_max = 10
-y_max = 10
+x_max = 1000
+y_max = 1000
 
 while (stri):
     index = stri.find(',')
@@ -59,38 +59,71 @@ for tup in vent_list:
         for x in range(np.minimum(x_1, x_2), np.maximum(x_1, x_2)+1):
             field[x, y_1] += 1
             print('\t\t', x, y_1)
-    elif x_2 - x_1 == y_2 - y_1:  # diagonal line in the "intuitive" direction
+
+    elif np.abs(x_2 - x_1) == np.abs(y_2 - y_1):
         print('\tcovers')
-        d = x_2 - x_1
-        if d >= 0:
-            for i in range(d+1):
-                field[x_1 + i, y_1 + i] += 1
-                print('\t\t', x_1 + i, y_1 + i)
-                # pdb.set_trace()
-        elif d <= 0:
-            for i in range(-d+1):
-                field[x_2 + i, y_2 + i] += 1
-                print('\t\t', x_2 + i, y_2 + i)
-                # pdb.set_trace()
-    elif x_2 - x_1 == y_1 - y_2:  # diagonal line in the "counterintuitive" direction
-        print('\tcovers')
-        d = x_2 - x_1
-        if d >= 0:
-            for i in range(d+1):
-                field[x_1 + i, y_2 + i] += 1
-                print('\t\t', x_1 + i, y_2 + i)
-        elif d > 0:
-            for i in range(-d+1):
-                field[x_2 + i, y_1 + i] += 1
-                print('\t\t', x_2 + i, y_1 + i)
+
+        y_ascending = True if y_2 - y_1 >= 0 else False  # case ==0 does not occur
+        x_ascending = True if x_2 - x_1 >= 0 else False  # case ==0 does not occur
+
+        current_x   = x_1
+        current_y   = y_1
+
+        field[current_x, current_y] += 1
+        print('\t\t', current_x, current_y)
+
+        while(True):
+            if x_ascending:
+                current_x += 1
+            else:
+                current_x -= 1
+
+            if y_ascending:
+                current_y += 1
+            else:
+                current_y -= 1
+
+            field[current_x, current_y] += 1
+            print('\t\t', current_x, current_y)
+
+            if current_x == x_2:
+                assert(current_y == y_2), 'Logical error in diagonal'
+                break
+
+
+
+    # elif x_2 - x_1 == y_2 - y_1:  # diagonal line in the "intuitive" direction
+    #     print('\tcovers')
+    #     d = x_2 - x_1
+    #     if d >= 0:
+    #         for i in range(d+1):
+    #             field[x_1 + i, y_1 + i] += 1
+    #             print('\t\t', x_1 + i, y_1 + i)
+    #             # pdb.set_trace()
+    #     elif d <= 0:
+    #         for i in range(-d+1):
+    #             field[x_2 + i, y_2 + i] += 1
+    #             print('\t\t', x_2 + i, y_2 + i)
+    #             # pdb.set_trace()
+    # elif x_2 - x_1 == y_1 - y_2:  # diagonal line in the "counterintuitive" direction
+    #     print('\tcovers')
+    #     d = x_2 - x_1
+    #     if d >= 0:
+    #         for i in range(d+1):
+    #             field[x_1 + i, y_2 + i] += 1
+    #             print('\t\t', x_1 + i, y_2 + i)
+    #     elif d > 0:
+    #         for i in range(-d+1):
+    #             field[x_2 + i, y_1 + i] += 1
+    #             print('\t\t', x_2 + i, y_1 + i)
 
 
 # final computations:
 dangerous = np.where(field > 1, 1, 0)
 n_dangerous = dangerous.sum()
 
-print(field)
+# print(field.T)  # transpose so that the view is similar as in the puzzle
+# print(dangerous.T)  # transpose so that the view is similar as in the puzzle
 
-print(dangerous)
-
-print(n_dangerous)
+print('--------------------')
+print('Number of dangerous spots:', n_dangerous)

9289c76e6e00f7126c639e36852c284b5ac65eed
justus <j-gk@gmx.net>
2021-12-14 09:49:31 +0100

    Solved day 14 part 2
    With a hint from Tobias, I managed to find the elegant way of counting.

--- 14-2-prog.py
+++ 14-2-prog.py
@@ -1,43 +1,83 @@
-with open('14-1-sample_input.txt', 'r') as f:
+import pdb  # Python debugger
+
+
+with open('14-1-input.txt', 'r') as f:
     stri = f.read()
 
 index = stri.find('\n')
 polymer_template = stri[0:index]
 stri = stri[index+2:]
 
-insertion_rules = {}
-occurences      = {}
+insertion_rules  = {}
+occurences_pairs = {}
+occurences_pairs_temp = {}
+occurences       = {}       # for number of occurences of each letter
+letter_pairs = []
+letters = []
 while(stri):
     left                  = stri[0]
     right                 = stri[1]
     letter_pair           = stri[0:2]
     inserted              = stri[6]
-    insertion_rules[letter_pair] = inserted
+    insertion_rules[letter_pair]  = inserted
 
-    occurences[left]      = 0
-    occurences[right]     = 0
+    occurences[left] = 0
+    occurences[right] = 0                   # initialize the keys of occurences
+    letters.append(left)
+    letters.append(right)
+
+    occurences_pairs[letter_pair] = 0       # initialize the keys of occurences_pairs
+    occurences_pairs_temp[letter_pair] = 0
+    letter_pairs.append(letter_pair)
 
     stri                  = stri[8:]
 
-def recursive(left, right, depth):
-    if depth == 23:
-        print(left, end='')
-        occurences[left] += 1
-    else:
-        # assert(depth < 1)
-        inserted = insertion_rules[left + right]
+# initialize values of occurences_pairs
+for index in range(len(polymer_template) -1):
+    letter_pair = polymer_template[index:index+2]
+    occurences_pairs[letter_pair] += 1
 
-        recursive(left, inserted, depth+1)
-        recursive(inserted, right, depth+1)
+# pdb.set_trace()
 
-recursive('N', 'N', 0)
-recursive('N', 'C', 0)
-recursive('C', 'B', 0)
 
-print('B', end='\n')
-occurences['B'] += 1
+for step in range(1, 41):
 
-print(occurences)
+    # prepare temporal dictionary for use
+    for letter_pair in letter_pairs:
+        occurences_pairs_temp[letter_pair] = 0
+
+    # pdb.set_trace()
+
+    # iteration step:
+    for letter_pair in letter_pairs:
+        left = letter_pair[0]
+        right = letter_pair[1]
+        inserted = insertion_rules[letter_pair]
+
+        occurences_pairs_temp[left + inserted] += occurences_pairs[letter_pair]
+        occurences_pairs_temp[inserted + right] += occurences_pairs[letter_pair]
+
+    # pdb.set_trace()
+
+    occurences_pairs = occurences_pairs_temp.copy()
+
+    print('After', step, 'steps:\n', occurences_pairs)
+
+
+# Go back from pairs of letters to single letters. For this, count only the left
+# letter of each pair. In the end, we have to add the rightmost letter of the
+# input once.
+
+for letter_pair in letter_pairs:
+    left = letter_pair[0]
+    occurences[left] += occurences_pairs[letter_pair]
+
+rightmost = polymer_template[-1]
+occurences[rightmost] += 1
+
+print('Result:', occurences, sep='\n')
+
 max = max(occurences.values())
 min = min(occurences.values())
+
 print('Puzzle solution is ', max, '-', min, ' = ', max-min, sep='')
